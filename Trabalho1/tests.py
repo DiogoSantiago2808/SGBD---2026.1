e293a473c306f556affe6caa01a8d715a35dc66f
Xantanio <paulo.janio1291@gmail.com>
2026-03-29 12:36:23 -0300

    criação dos testes pedidos no trabalho

--- Trabalho1/tests.py
+++ Trabalho1/tests.py
@@ -121,18 +121,23 @@ def test_simulacao_experimental():
 
    
     print("\n📌 Inserções obrigatórias:")
-    insert_keys = [23, 48, 41, 42]
+    insert_keys = [
+        (18, "R18"), (22, "R22"), (27, "R27"), (35, "R35"),
+        (41, "R41"), (44, "R44"), (63, "R63"), (67, "R67"),
+        (83, "R83"), (86, "R86"), (121, "R121"), (145, "R145")
+    ]
 
     for key in insert_keys:
+        chave = key[0]
         print(f"Inserindo {key}...")
-        isam.insert(key)
+        isam.insert(chave)
 
     print("\n📌 Estrutura após inserções:")
     isam.print_structure()
 
     
     print("\n📌 Remoções:")
-    remove_keys = [23, 41]
+    remove_keys = [27, 44, 67, 83, 145]
 
     for key in remove_keys:
         print(f"Removendo {key}...")
@@ -144,32 +149,50 @@ def test_simulacao_experimental():
   
     print("\n📌 Busca por igualdade:")
 
-    key = 42
-    leaf, path = isam.find_leaf(key)
+    key_search = [22, 35, 44, 90]
+    for key in key_search:
+        leaf, path = isam.find_leaf(key)
 
-    print(f"\nBuscando {key}...")
-    print("Caminho percorrido:")
-    for node in path:
-        print(node.keys)
+        print(f"\n--- Analisando Busca({key}) ---")
+        print("Caminho percorrido nos índices:")
+        
+        for node in path:
+            print(f" -> Nó: {node.keys}")
 
-    result = isam.search(key)
-    print("Encontrado:", result)
+        result = isam.search(key)
+        print(f"Encontrado: {'Sim' if result else 'Não'}")
 
-   
+        custo = isam.metrics[-1] if isam.metrics else 0
+        print(f"Custo total da busca: {custo} nós/páginas")
+
     print("\n📌 Busca por intervalo:")
+    intervalos = [(20, 50), (60, 90), (120, 150)]
 
-    start, end = 20, 50
-    leaf, path = isam.find_leaf(start)
+    for start, end in intervalos:
+        leaf, path = isam.find_leaf(start)
 
-    print(f"\nIntervalo {start} até {end}")
-    print("Caminho inicial:")
-    for node in path:
-        print(node.keys)
+        print(f"\n--- Analisando Intervalo({start} até {end}) ---")
+        print("Caminho percorrido até o início do intervalo:")
+        
+        for node in path:
+            print(f" -> Nó: {node.keys}")
+
+        result = isam.range_search(start, end)
+        
+        print(f"Registros encontrados: {result}")
+        print(f"Total de registros: {len(result)}")
+
+        custo = isam.metrics[-1] if isam.metrics else 0
+        print(f"Custo total da busca por intervalo: {custo} nós/páginas")
 
     result = isam.range_search(start, end)
     print("Resultado:", result)
+    custo = isam.metrics[-1] if isam.metrics else 0
+    print(f"Intervalo({start}, {end}): {len(result)} registros | Custo: {custo} nós")
 
-  
+    print("\n📌 Estrutura todas as operações:")
+    isam.print_structure()
+
     print("\n📌 Métricas:")
     isam.show_metrics()
 
